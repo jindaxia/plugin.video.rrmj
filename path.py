faea8d5919dbeaa878125ad0f0c377118ce029a4
虾哥哥 <jfk_jin@sina.com>
2016-03-21 14:52:14 +0800

    fix album error

--- path.py
+++ path.py
@@ -167,12 +167,19 @@ def search(page, **kwargs):
 def get_album(albumId):
     c_list = Meiju.get_album(albumId)
     for one in c_list["data"]["results"]:
-        item = ListItem({
+        item = ListItem(**{
             'label': one.get("title"),
             'path': plugin.url_for("detail", seasonId=one.get("id")),
-            'is_playable': False
+            'icon': one["cover"],
+            'thumbnail': one["cover"],
         })
+        item.set_info("video", {"plot": one.get("brief", ""),
+                                "rating ": float(one["score"]),
+                                "genre": one["cat"],
+                                "season": one["seasonNo"]})
+        item.set_is_playable(False)
         yield item
+    plugin.set_content('TVShows')
 
 
 # get season episodes by season id
