3f57ba03837e02821d3245c35b5944aba1b7af99
wilson <wilson.wen.chn@gmail.com>
2016-11-25 09:43:32 +0800

    update get_by_sid url
    update url for "findM3u8ByEpisodeSid"

--- resources/lib/rrmj.py
+++ resources/lib/rrmj.py
@@ -120,7 +120,7 @@ class RenRenMeiJu(object):
 class RRMJResolver(RenRenMeiJu):
 
     def get_by_sid(self, **kwargs):
-        API = "/v2/video/findM3u8ByEpisodeSid"
+        API = "/video/findM3u8ByEpisodeSid"
         data = self.get_json(SERVER + API, data=urllib.urlencode(kwargs), pretty=False)
         if data["code"] != "0000":
             return None, None
